d829718b88f9a7921162cc0b6d8d66aafaec2953
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-03-31 15:29:29 +0200

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-import React from 'react'
+import Home from './Components/Home';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import {AuthProvider} from './Components/Auth';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default App
 
